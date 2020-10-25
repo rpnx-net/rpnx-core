1463bc703ee96f5972f1d2cdfb2976bea169e297
Ryan P. Nicholl <rnicholl@protonmail.com>
2020-10-25 17:47:26 -0400

    Add RPNX_ASSERT

---
+++ public/headers/all/rpnx/assert.hpp
@@ -0,0 +1,29 @@
+#ifndef RPNX_ASSERT_HPP
+#define RPNX_ASSERT_HPP
+
+#include <assert.h>
+
+namespace rpnx
+{
+    namespace detail
+    {
+        inline void rpnx_assert_impl(char const * str, bool value)
+        {
+
+        }
+    }
+}
+
+#ifdef NDEBUG
+#if defined(__GNUC__) && defined(__cplusplus)
+#define RPNX_ASSERT(x) __builtin_assume(x)
+#elif defined(__clang__)
+#define RPNX_ASSERT(x) __builtin_assume(x)
+#elif defined(_MSC_VER)
+#define RPNX_ASSERT(x) __assume(x)
+#endif
+#else
+#define RPNX_ASSERT(x) assert(x)
+#endif
+
+#endif
