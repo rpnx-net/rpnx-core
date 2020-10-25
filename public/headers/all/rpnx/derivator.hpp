10058b995cb1c12d72921a0eb80464cdb1af9d17
Ryan P. Nicholl <rnicholl@protonmail.com>
2020-10-25 16:24:46 -0400

    Format and add destructor

--- public/headers/all/rpnx/derivator.hpp
+++ public/headers/all/rpnx/derivator.hpp
@@ -5,9 +5,9 @@
 #ifndef RPNX_DERIVATOR_HPP
 #define RPNX_DERIVATOR_HPP
 
-#include <type_traits>
 #include <memory>
 #include <tuple>
+#include <type_traits>
 #include <typeindex>
 #include <variant>
 
@@ -16,44 +16,65 @@
 namespace rpnx
 {
 
-    struct foo {};
+    struct foo
+    {
+    };
 
-
-    template <typename Allocator>
+    template < typename Allocator >
     struct derivator_vtab
     {
         std::type_index m_type_index = typeid(void);
-        void (*m_deleter)(Allocator const& a, typename std::allocator_traits<Allocator>::void_pointer) = nullptr;
-        void (*m_copier)(typename std::allocator_traits<Allocator>::const_void_pointer, typename std::allocator_traits<Allocator>::void_pointer) = nullptr;
-        bool (*m_equals)(typename std::allocator_traits<Allocator>::void_pointer, typename std::allocator_traits<Allocator>::void_pointer) = nullptr;
-        bool (*m_less)(void const*, void const *) = nullptr;
+        void (*m_deleter)(Allocator* a, typename std::allocator_traits< Allocator >::void_pointer) = nullptr;
+        void* (*m_construct)(Allocator* alloc, typename std::allocator_traits< Allocator >::const_void_pointer) = nullptr;
+        bool (*m_equals)(typename std::allocator_traits< Allocator >::void_pointer, typename std::allocator_traits< Allocator >::void_pointer) = nullptr;
+        bool (*m_less)(void const*, void const*) = nullptr;
         int m_index = -1;
     };
 
-    template <int I, typename T, typename Alloc>
-    constexpr derivator_vtab<Alloc> init_vtab_for()
+    namespace detail
     {
-        derivator_vtab<Alloc> tb;
-        tb.m_type_index = typeid(T);
-        // TODO: We assume actual void* here, change to use the allocator traits "void*" to support fancy pointers
-        tb.m_copier = [](void const* src, void * dest)
-        {
-            if constexpr (! std::is_void_v<T>)
-            {
-                new (dest) T(*reinterpret_cast<T const *>(src));
-            }
-        };
 
-        // TODO: We assume actual void* here, change to use the allocator traits "void*" to support fancy pointers
-        tb.m_deleter = [](Alloc const &a, void *val)
+        // There is some bug in visual studio that causes this not to work
+        template < typename T, typename Alloc >
+        void derivator_deletor(Alloc* alloc, void* src)
         {
-            if constexpr (! std::is_void_v<T>)
+            if constexpr (!std::is_void_v< T >)
             {
-                reinterpret_cast<T*>(val)->T::~T();
-            (typename std::allocator_traits<Alloc>::template rebind_alloc<T>(a)).deallocate(reinterpret_cast<T*>(val), sizeof(T));
+                ((T*)src)->T::~T();
+                (typename std::allocator_traits< Alloc >::template rebind_alloc< T >(*alloc)).deallocate((T*)src, sizeof(T));
             }
-        };
+        }
 
+        template <typename T, typename Alloc>
+        void * derivator_new(Alloc * alloc, void const * src)
+        {
+            if constexpr (!std::is_void_v< T >)
+            {
+                void* dest = (typename std::allocator_traits< Alloc >::template rebind_alloc< T >(*alloc)).allocate(sizeof(T));
+                try
+                {
+                    return (void*)new (dest) T(*reinterpret_cast< T const* >(src));
+                }
+                catch (...)
+                {
+                    (typename std::allocator_traits< Alloc >::template rebind_alloc< T >(*alloc)).deallocate((T*)dest, sizeof(T));
+                    throw;
+                }
+            }
+            else
+                return nullptr;
+        }
+    } // namespace detail
+    
+    
+    template < int I, typename T, typename Allocator >
+    derivator_vtab< Allocator > init_vtab_for()
+    {
+        derivator_vtab< Allocator > tb;
+        tb.m_type_index = typeid(T);        
+        tb.m_construct = &detail::derivator_new< T, Allocator >;
+        tb.m_deleter = &detail::derivator_deletor< T, Allocator >;
+        
         // TODO: It would be nice to support comparisons where possible, fix this.
         tb.m_equals = nullptr;
         tb.m_less = nullptr;
@@ -63,161 +84,149 @@ namespace rpnx
         return tb;
     }
 
-    template <int I, typename T, typename Alloc>
-    inline derivator_vtab<Alloc> derivator_vtab_v =  init_vtab_for<I, T, Alloc>();
+    template < int I, typename T, typename Allocator >
+    inline derivator_vtab< Allocator > derivator_vtab_v = init_vtab_for< I, T, Allocator >();
 
-    
-    template <typename Alloc, typename ... Types>
-    class basic_derivator
-    : private Alloc
+    template < typename Allocator, typename... Types >
+    class basic_derivator : private Allocator
     {
-        typename std::allocator_traits<Alloc>::void_pointer m_value;
-        derivator_vtab<Alloc> * m_vtab;
+        typename std::allocator_traits< Allocator >::void_pointer m_value;
+        derivator_vtab< Allocator >* m_vtab;
 
-    public:
-
-        basic_derivator()
-            :m_value(nullptr),
-              m_vtab(nullptr)
-        {
-            emplace<0>();
-        }
-
-
+      public:
+        basic_derivator() : m_value(nullptr), m_vtab(nullptr) { emplace< 0 >(); }
+        ~basic_derivator() { destroy(); }
 
         void destroy()
         {
             if (m_value && m_vtab)
             {
-                m_vtab->m_deleter((Alloc&)*this, m_value);
+                m_vtab->m_deleter(this, m_value);
                 m_value = nullptr;
             }
-
         }
 
-
-        template <typename T, typename ...Ts>
-        void emplace (Ts && ... ts)
+        template < typename T, typename... Ts >
+        void emplace(Ts&&... ts)
         {
-            emplace< tuple_type_index<T, std::tuple<Types...>>::value >(std::forward<Ts>(ts)...);
+            emplace< tuple_type_index< T, std::tuple< Types... > >::value >(std::forward< Ts >(ts)...);
         }
 
-        template <size_t I, typename ... Ts>
-        void emplace(Ts &&  ...ts)
+        template < size_t I, typename... Ts >
+        void emplace(Ts&&... ts)
         {
-            
-
-            if constexpr(std::is_void_v< std::tuple_element_t<I, std::tuple<Types...>> >)
+            if constexpr (std::is_void_v< std::tuple_element_t< I, std::tuple< Types... > > >)
             {
                 destroy();
-                m_vtab = &derivator_vtab_v<I,  std::tuple_element_t<I, std::tuple<Types...>>, Alloc>;
+                m_vtab = &derivator_vtab_v< I, std::tuple_element_t< I, std::tuple< Types... > >, Allocator >;
             }
             else
             {
-                
-                auto ptr = (typename std::allocator_traits<Alloc>::template rebind_alloc<std::tuple_element_t<I, std::tuple<Types...>>>((Alloc&)*this)).allocate(sizeof(std::tuple_element_t<I, std::tuple<Types...>>));
+                using T = std::tuple_element_t< I, std::tuple< Types... > >;
+                void* ptr = (typename std::allocator_traits< Allocator >::template rebind_alloc< T >((Allocator&)*this)).allocate(sizeof(std::tuple_element_t< I, std::tuple< Types... > >));
                 try
                 {
-                    new (ptr) std::tuple_element_t<I, std::tuple<Types...>>(std::forward<Ts>(ts)...);
+                    ptr = (void*)new (ptr) T(std::forward< Ts >(ts)...);
                 }
                 catch (...)
                 {
-                    (typename std::allocator_traits<Alloc>::template rebind_alloc<std::tuple_element_t<I, std::tuple<Types...>>>((Alloc&)*this)).deallocate(ptr, sizeof(std::tuple_element_t<I, std::tuple<Types...>>));
+                    (typename std::allocator_traits< Allocator >::template rebind_alloc< T >((Allocator&)*this)).deallocate((T*)ptr, sizeof(std::tuple_element_t< I, std::tuple< Types... > >));
                     throw;
                 }
+
                 destroy();
-                m_vtab = &derivator_vtab_v<I,  std::tuple_element_t<I, std::tuple<Types...>>, Alloc>;
-                m_value = reinterpret_cast<void*>(ptr);
+                m_vtab = &derivator_vtab_v< I, std::tuple_element_t< I, std::tuple< Types... > >, Allocator >;
+                m_value = reinterpret_cast< void* >(ptr);
             }
         }
 
-        std::size_t index() const noexcept
-        {
-           return m_vtab->m_index;
-        }
+        std::size_t index() const noexcept { return m_vtab->m_index; }
 
-        template <int I>
-        std::tuple_element_t<I, std::tuple<Types...>> & as()
+        template < int I >
+        std::tuple_element_t< I, std::tuple< Types... > >& as()
         {
             if (m_vtab->m_index != I) throw std::invalid_argument("derivator");
 
-            return *reinterpret_cast<std::tuple_element_t<I, std::tuple<Types...>>*>(m_value);
+            return *reinterpret_cast< std::tuple_element_t< I, std::tuple< Types... > >* >(m_value);
         }
 
-        template <int I>
-        std::tuple_element_t<I, std::tuple<Types...>> const & as() const
+        template < int I >
+        std::tuple_element_t< I, std::tuple< Types... > > const& as() const
         {
             if (m_vtab->m_index != I) throw std::invalid_argument("derivator");
 
-            return *reinterpret_cast<std::tuple_element_t<I, std::tuple<Types...>> const*>(m_value);
+            return *reinterpret_cast< std::tuple_element_t< I, std::tuple< Types... > > const* >(m_value);
         }
 
-        template <typename T>
-        auto & as()
+        template < typename T >
+        auto& as()
         {
-            static_assert(tuple_type_index<T, std::tuple<Types...>>::value != -1, "The type T in derivator<Types...>::as<T>() is not present in Types...");
-            return as<tuple_type_index<T, std::tuple<Types...>>::value>();
+            static_assert(tuple_type_index< T, std::tuple< Types... > >::value != -1, "The type T in derivator<Types...>::as<T>() is not present "
+                                                                                      "in Types...");
+            return as< tuple_type_index< T, std::tuple< Types... > >::value >();
         }
 
-        template <typename T>
-        auto const & as() const
+        template < typename T >
+        auto const& as() const
         {
-            static_assert(tuple_type_index<T, std::tuple<Types...>>::value != -1, "The type T in derivator<Types...>::as<T>() is not present in Types...");
-            return as<tuple_type_index<T, std::tuple<Types...>>::value>();
+            static_assert(tuple_type_index< T, std::tuple< Types... > >::value != -1, "The type T in derivator<Types...>::as<T>() is not present "
+                                                                                      "in Types...");
+            return as< tuple_type_index< T, std::tuple< Types... > >::value >();
         }
 
-        template <int I>
-        std::tuple_element_t<I, std::tuple<Types...>> & as_unchecked()
+        template < int I >
+        std::tuple_element_t< I, std::tuple< Types... > >& as_unchecked()
         {
-            return *reinterpret_cast<std::tuple_element_t<I, std::tuple<Types...>>*>(m_value);
+            return *reinterpret_cast< std::tuple_element_t< I, std::tuple< Types... > >* >(m_value);
         }
 
-        template <int I>
-        std::tuple_element_t<I, std::tuple<Types...>> const & as_unchecked() const
+        template < int I >
+        std::tuple_element_t< I, std::tuple< Types... > > const& as_unchecked() const
         {
-            return *reinterpret_cast<std::tuple_element_t<I, std::tuple<Types...>> const*>(m_value);
+            return *reinterpret_cast< std::tuple_element_t< I, std::tuple< Types... > > const* >(m_value);
         }
 
-        template <typename T>
-        auto & as_unchecked()
+        template < typename T >
+        auto& as_unchecked()
         {
-            static_assert(tuple_type_index<T, std::tuple<Types...>>::value != -1, "The type T in derivator<Types...>::as<T>() is not present in Types...");
-            return as_unchecked<tuple_type_index<T, std::tuple<Types...>>::value>();
+            static_assert(tuple_type_index< T, std::tuple< Types... > >::value != -1, "The type T in derivator<Types...>::as<T>() is not present "
+                                                                                      "in Types...");
+            return as_unchecked< tuple_type_index< T, std::tuple< Types... > >::value >();
         }
 
-        template <typename T>
-        auto const & as_unchecked() const
+        template < typename T >
+        auto const& as_unchecked() const
         {
-            static_assert(tuple_type_index<T, std::tuple<Types...>>::value != -1, "The type T in derivator<Types...>::as<T>() is not present in Types...");
-            return as_unchecked<tuple_type_index<T, std::tuple<Types...>>::value>();
+            static_assert(tuple_type_index< T, std::tuple< Types... > >::value != -1, "The type T in derivator<Types...>::as<T>() is not present "
+                                                                                      "in Types...");
+            return as_unchecked< tuple_type_index< T, std::tuple< Types... > >::value >();
         }
 
-        void swap(basic_derivator<Alloc, Types...>  & other) noexcept
+        void swap(basic_derivator< Allocator, Types... >& other) noexcept
         {
-            // TODO: this should only be noexcept if the allocator void_pointer type is noexcept swappable
+            // TODO: this should only be noexcept if the allocator void_pointer type is
+            // noexcept swappable
             std::swap(m_vtab, other.m_vtab);
             std::swap(m_value, other.m_value);
         }
 
-        template <typename T>
+        template < typename T >
         bool holds_alternative() const noexcept
         {
-            return tuple_type_index<T, std::tuple<Types...>>::value == m_vtab->m_index;
+            return tuple_type_index< T, std::tuple< Types... > >::value == m_vtab->m_index;
         }
-
-
     };
 
-   
-    /** The class rpnx::derivator is a bit like std::variant, except that it allocates memory indirectly.
-     * This allows derivators that can refer to the enclosing class, and also can save memory if some possible 
-     * values are much larger than others (variant always allocates enough space for the largest). If you 
-     * need control over memory allocation, use rpnx::basic_derivator<Allocator, Types...>
-     * 
+    /** The class rpnx::derivator is a bit like std::variant, except that it
+     * allocates memory indirectly. This allows derivators that can refer to the
+     * enclosing class, and also can save memory if some possible values are much
+     * larger than others (variant always allocates enough space for the largest).
+     * If you need control over memory allocation, use
+     * rpnx::basic_derivator<Allocator, Types...>
+     *
      * */
-    template <typename ... Ts>
-    using derivator = basic_derivator<std::allocator<void>, Ts...>;
-}
-
+    template < typename... Ts >
+    using derivator = basic_derivator< std::allocator< void >, Ts... >;
+} // namespace rpnx
 
 #endif
