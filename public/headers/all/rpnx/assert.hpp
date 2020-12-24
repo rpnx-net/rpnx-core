4e30a1cf16bba200edb9d21391c5a894534d02e2
Ryan P. Nicholl <rnicholl@protonmail.com>
2020-12-23 21:17:03 -0500

    Fix bug in RPNX_ASSERT macro.

--- public/headers/all/rpnx/assert.hpp
+++ public/headers/all/rpnx/assert.hpp
@@ -21,6 +21,8 @@ namespace rpnx
 #define RPNX_ASSERT(x) __builtin_assume(x)
 #elif defined(_MSC_VER)
 #define RPNX_ASSERT(x) __assume(x)
+#else
+#define RPNX_ASSERT(x)
 #endif
 #else
 #define RPNX_ASSERT(x) assert(x)
