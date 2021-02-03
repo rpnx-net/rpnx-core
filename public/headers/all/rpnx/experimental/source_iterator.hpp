6ae20f0c188a3a42bca07f36f3d95283cc3ec7e7
Ryan P. Nicholl <rnicholl@protonmail.com>
2021-02-02 20:24:01 -0500

    Add source iterator

---
+++ public/headers/all/rpnx/experimental/source_iterator.hpp
@@ -0,0 +1,153 @@
+//
+// Created by Ryan on 2/2/2021.
+//
+
+#ifndef RPNXCORE_SOURCE_ITERATOR_HPP
+#define RPNXCORE_SOURCE_ITERATOR_HPP
+
+#include <memory>
+#include <string>
+
+namespace rpnx::experimental
+{
+    template < typename It >
+    class source_iterator
+    {
+        It m_iter;
+        std::shared_ptr< std::string > m_filename;
+
+        std::size_t m_line = 0;
+        std::size_t m_col = 0;
+        enum class state
+        {
+            text,
+            r,
+            n
+        };
+        state m_state = state::text;
+
+      public:
+        source_iterator(It val = It(), std::string fname = "") : m_iter(val), m_filename(std::make_shared< std::string >(fname))
+        {
+        }
+        source_iterator(source_iterator< It > const&) = default;
+        source_iterator(source_iterator< It >&&) = default;
+
+        source_iterator< It >& operator=(source_iterator< It > const&) = default;
+        source_iterator< It >& operator=(source_iterator< It >&&) = default;
+
+        bool operator==(source_iterator< It > const& other) const
+        {
+            return m_iter == other.m_iter;
+        }
+        bool operator!=(source_iterator< It > const& other) const
+        {
+            return m_iter != other.m_iter;
+        }
+        bool operator<(source_iterator< It > const& other) const
+        {
+            return m_iter < other.m_iter;
+        }
+        bool operator<=(source_iterator< It > const& other) const
+        {
+            return m_iter <= other.m_iter;
+        }
+        bool operator>(source_iterator< It > const& other) const
+        {
+            return m_iter > other.m_iter;
+        }
+        bool operator>=(source_iterator< It > const& other) const
+        {
+            return m_iter >= other.m_iter;
+        }
+
+        source_iterator< It >& operator++()
+        {
+            if (m_state == state::text)
+            {
+                if (*m_iter == '\r')
+                {
+                    m_state = state::r;
+                }
+                else if (*m_iter == '\n')
+                {
+                    m_state = state::n;
+                }
+            }
+            else if (m_state == state::r)
+            {
+                if (*m_iter == '\r')
+                {
+                    m_line++;
+                    m_col = 0;
+                }
+                else if (*m_iter == '\n')
+                {
+                    m_state = state::n;
+                }
+                else
+                {
+                    m_state = state::text;
+                    m_line++;
+                    m_col = 0;
+                }
+            }
+            else if (m_state == state::n)
+            {
+                m_line++;
+                m_col = 0;
+                if (*m_iter == '\r')
+                {
+                    m_state = state::r;
+                }
+                else if (*m_iter == '\n')
+                {
+                    m_state = state::n;
+                }
+                else
+                {
+                    m_state = state::text;
+                }
+            }
+
+            return *this;
+        }
+        source_iterator< It > operator++(int)
+        {
+            source_iterator< It > copy = *this;
+            ++*this;
+            return copy;
+        }
+
+        std::size_t line() const
+        {
+            return m_line;
+        }
+        std::size_t column() const
+        {
+            return m_col;
+        }
+        std::string name() const
+        {
+            return *m_filename;
+        }
+
+        // TODO: figure out how to implement this efficiently
+        /*
+         *
+        source_iterator<It>& operator--()
+        {
+            m_iter --;
+
+            if ((*m_iter == '\r' || m_iter == '\n') && !(*m_iter == '\r' && m_state == '\n'))
+            {
+                m
+            }
+
+            return *this;
+        }
+         */
+    };
+} // namespace rpnx::experimental
+
+#endif // RPNXCORE_SOURCE_ITERATOR_HPP
